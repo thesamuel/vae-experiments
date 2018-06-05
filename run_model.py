c634ef2abdaf855867e63f44c2a002059c266995
Sam Gehman <sgehman@cs.uw.edu>
2018-06-05 16:41:59 -0700

    Update run model for CNN

--- run_model.py
+++ run_model.py
@@ -1,42 +1,43 @@
-import sys
-import argparse
 from tqdm import tqdm
 
 import torch
 import torch.utils.data
 from torchvision import datasets, transforms
-from torchvision.utils import save_image
 
-from models import M1, M2
+# from models import M1, M2
+from cnn_model import M1
 
 
 def get_data(batch_size):
     # kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
     kwargs = {}
     train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data/', train=True, download=True,
-                transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True, **kwargs)
+        datasets.MNIST('data/', train=True, download=True,
+                       transform=transforms.ToTensor()),
+        batch_size=batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data/', train=False, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True, **kwargs)
+        datasets.MNIST('data/', train=False, transform=transforms.ToTensor()),
+        batch_size=batch_size, shuffle=True, **kwargs)
     return train_loader, test_loader
+
 
 batch_size = 128
 train_loader, test_loader = get_data(batch_size)
 network_arch = {}
-# M1_model = M1(network_arch)
-# M1_model.model.train()
-M2_model = M2(network_arch)
-M2_model.model.train()
 
-M2_model.model.gen_samples()
+m = M1(network_arch)
+m.model.train()
+
+# m = M2(network_arch)
+# m.model.train()
+
+m.model.gen_samples()
 for epoch in range(50):
     train_loss = 0
     for i, (x, y) in enumerate(tqdm(train_loader)):
-        output = M2_model.fit(x, y)
+        output = m.fit(x)
         train_loss += output['loss'].item()
     print('epoch loss:', train_loss / len(train_loader.dataset))
-    M2_model.model.eval()
-    M2_model.model.gen_samples()
-    M2_model.model.train()
+    m.model.eval()
+    m.model.gen_samples()
+    m.model.train()
